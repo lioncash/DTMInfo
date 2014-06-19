4eba9da4fabdd81eb4c4ddaf201a05a9fad16be4
Lioncash <mathew1800@gmail.com>
2014-06-19 01:25:54 -0400

    Initial commit

---
+++ DTMInfo/DTM.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DTMInfo
+{
+	/// <summary>
+	/// Dolphin Movie File.
+	/// </summary>
+	public sealed class DTM
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="filename">Path to the DTM movie file.</param>
+		public DTM(string filename)
+		{
+			if (filename == null)
+				throw new ArgumentNullException("filename");
+
+			if (!File.Exists(filename))
+				throw new IOException("Specified file does not exist.");
+
+			BinaryReader reader = new BinaryReader(File.OpenRead(filename));
+
+			byte[] fileMagic = reader.ReadBytes(4);
+			if (!IsValidHeaderID(fileMagic))
+				throw new IOException("Invalid file: Header magic is incorrect.");
+
+			GameID                      = Encoding.UTF8.GetString(reader.ReadBytes(6));
+			IsWiiGame                   = reader.ReadBoolean();
+			ConnectedControllers        = reader.ReadByte();
+			IsFromSaveState             = reader.ReadBoolean();
+			FrameCount                  = reader.ReadUInt64();
+			InputFrameCount             = reader.ReadUInt64();
+			LagFrameCount               = reader.ReadUInt64();
+			UniqueID                    = reader.ReadUInt64();
+			NumRerecords                = reader.ReadUInt32();
+			Author                      = Encoding.UTF8.GetString(reader.ReadBytes(32));
+			VideoBackend                = Encoding.UTF8.GetString(reader.ReadBytes(16));
+			AudioEmulator               = Encoding.UTF8.GetString(reader.ReadBytes(16));
+			MD5                         = ExtractRevisionOrMD5(reader.ReadBytes(16));
+			RecordingStartTime          = reader.ReadUInt64();
+			IsSavedConfig               = reader.ReadBoolean();
+			UsingIdleSkip               = reader.ReadBoolean();
+			UsingDualCore               = reader.ReadBoolean();
+			UsingProgressiveScan        = reader.ReadBoolean();
+			UsingHLEDSP                 = reader.ReadBoolean();
+			UsingFastDiscSpeed          = reader.ReadBoolean();
+			CPUCore                     = GetCPUCoreName(reader.ReadByte());
+			IsEFBAccessEnabled          = reader.ReadBoolean();
+			IsEFBCopiesEnabled          = reader.ReadBoolean();
+			UsingEFBToTexture           = reader.ReadBoolean();
+			IsEFBCopyCacheEnabled       = reader.ReadBoolean();
+			IsEmulatingEFBFormatChanges = reader.ReadBoolean();
+			UsingXFB                    = reader.ReadBoolean();
+			UsingRealXFB                = reader.ReadBoolean();
+			UsingMemoryCard             = reader.ReadBoolean();
+			UsingClearSaves             = reader.ReadBoolean();
+			Bongos                      = reader.ReadByte();
+			SyncGPU                     = reader.ReadBoolean();
+			UsingNetplay                = reader.ReadBoolean();
+
+			// Skip reserved bytes
+			reader.BaseStream.Position += 13;
+
+			SecondDiscName              = Encoding.UTF8.GetString(reader.ReadBytes(40));
+			GitRevision                 = ExtractRevisionOrMD5(reader.ReadBytes(20)); // TODO: This will likely need to be manually converted.
+			DSPIROMHash                 = reader.ReadUInt32();
+			DSPCoefHash                 = reader.ReadUInt32();
+
+			// Skip reserved bytes.
+			reader.BaseStream.Position += 19;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// ID of the game that this movie was made for.
+		/// </summary>
+		public string GameID { get; private set; }
+
+		/// <summary>
+		/// Whether or not this movie file is for a Wii game.
+		/// </summary>
+		public bool IsWiiGame { get; private set; }
+
+		/// <summary>
+		/// Number of connected controllers (1-4)
+		/// </summary>
+		public int ConnectedControllers { get; private set; }
+
+		/// <summary>
+		/// false indicates that the recording started from bootup, true for savestate
+		/// </summary>
+		public bool IsFromSaveState { get; private set; }
+
+		/// <summary>
+		/// Number of frames in the recording.
+		/// </summary>
+		public ulong FrameCount { get; private set; }
+
+		/// <summary>
+		/// Number of input frames in the recording.
+		/// </summary>
+		public ulong InputFrameCount { get; private set; }
+
+		/// <summary>
+		/// Number of lag frames in the recording.
+		/// </summary>
+		public ulong LagFrameCount { get; private set; }
+
+		/// <summary>
+		/// (not implemented) A Unique ID comprised of: md5(time + Game ID)
+		/// </summary>
+		public ulong UniqueID { get; private set; }
+
+		/// <summary>
+		/// Number of rerecords/'cuts' of this TAS
+		/// </summary>
+		public uint NumRerecords { get; private set; }
+
+		/// <summary>
+		/// Author's name
+		/// </summary>
+		public string Author { get; private set; }
+
+		/// <summary>
+		/// Name of the video backend used.
+		/// </summary>
+		public string VideoBackend { get; private set; }
+
+		/// <summary>
+		/// Name of the audio emulator used.
+		/// </summary>
+		public string AudioEmulator { get; private set; }
+
+		/// <summary>
+		/// MD5 of the game ISO.
+		/// </summary>
+		public string MD5 { get; private set; }
+
+		/// <summary>
+		/// Seconds since 1970 that recording started (used for RTC).
+		/// </summary>
+		public ulong RecordingStartTime { get; private set; }
+
+		/// <summary>
+		/// Whether or not certain settings are loaded.
+		/// </summary>
+		/// <remarks>
+		/// All properties following this one are loaded during startup if true.
+		/// </remarks>
+		public bool IsSavedConfig { get; private set; }
+
+
+		public bool UsingIdleSkip               { get; private set; }
+		public bool UsingDualCore               { get; private set; }
+		public bool UsingProgressiveScan        { get; private set; }
+		public bool UsingHLEDSP                 { get; private set; }
+		public bool UsingFastDiscSpeed          { get; private set; }
+
+		/// <summary>CPU core type used. 0 = Interpreter, 1 = JIT, 2 = JITIL</summary>
+		public string CPUCore                   { get; private set; }
+		public bool IsEFBAccessEnabled          { get; private set; }
+		public bool IsEFBCopiesEnabled          { get; private set; }
+		public bool UsingEFBToTexture           { get; private set; }
+		public bool IsEFBCopyCacheEnabled       { get; private set; }
+		public bool IsEmulatingEFBFormatChanges { get; private set; }
+		public bool UsingXFB                    { get; private set; }
+		public bool UsingRealXFB                { get; private set; }
+		public bool UsingMemoryCard             { get; private set; }
+
+		/// <summary>Create a new memory card when playing back a movie if true</summary>
+		public bool UsingClearSaves             { get; private set; }
+		public byte Bongos                      { get; private set; }
+		public bool SyncGPU                     { get; private set; }
+		public bool UsingNetplay                { get; private set; }
+
+		/// <summary>Name of ISO file to switch to for two disc games.</summary>
+		public string SecondDiscName            { get; private set; }
+		public string GitRevision               { get; private set; }
+		public uint DSPIROMHash                 { get; private set; }
+		public uint DSPCoefHash                 { get; private set; }
+		public byte Reserved2                   { get; private set; }
+
+		#endregion
+
+		#region Utility Methods
+
+		private static bool IsValidHeaderID(byte[] data)
+		{
+			return data[0] == 'D' &&
+			       data[1] == 'T' &&
+			       data[2] == 'M' &&
+			       data[3] == 0x1A;
+		}
+
+		private static string GetCPUCoreName(byte value)
+		{
+			switch (value)
+			{
+				case 0:
+					return "Interpreter";
+				
+				case 1:
+					return "JIT";
+
+				case 2:
+					return "JITIL";
+
+				default:
+					return "Unknown";
+			}
+		}
+
+		private static string ExtractRevisionOrMD5(byte[] data)
+		{
+			StringBuilder sb = new StringBuilder(256);
+
+			foreach (byte b in data)
+			{
+				sb.Append(b.ToString("X"));
+			}
+
+			return sb.ToString();
+		}
+
+		#endregion
+	}
+}
---
+++ DTMInfo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DTMInfo
+{
+	internal sealed class Program
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Usage: DTMInfo.exe [path to DTM movie file]");
+				return;
+			}
+
+			DTM movieFile = new DTM(args[0]);
+
+			Console.WriteLine("Game ID                        : {0}", movieFile.GameID);
+			Console.WriteLine("Is Wii game                    : {0}", movieFile.IsWiiGame);
+			Console.WriteLine("Number of connected controllers: {0}", movieFile.ConnectedControllers);
+			Console.WriteLine("Is from a save state           : {0}", movieFile.IsFromSaveState);
+			Console.WriteLine("Frame count                    : {0}", movieFile.FrameCount);
+			Console.WriteLine("Input frames                   : {0}", movieFile.InputFrameCount);
+			Console.WriteLine("Lag frames                     : {0}", movieFile.LagFrameCount);
+			Console.WriteLine("Number of re-records           : {0}", movieFile.NumRerecords);
+			Console.WriteLine("Author name                    : {0}", movieFile.Author);
+			Console.WriteLine("Video backend                  : {0}", movieFile.VideoBackend);
+			Console.WriteLine("Audio emulator                 : {0}", movieFile.AudioEmulator);
+			Console.WriteLine("MD5                            : {0}", movieFile.MD5
+				);
+			Console.WriteLine("Recording start time           : {0} seconds", movieFile.RecordingStartTime);
+			Console.WriteLine("Using a saved config           : {0}", movieFile.IsSavedConfig);
+			Console.WriteLine("Using idle skipping            : {0}", movieFile.UsingIdleSkip);
+			Console.WriteLine("Using dual core                : {0}", movieFile.UsingDualCore);
+			Console.WriteLine("Using progressive scan         : {0}", movieFile.UsingProgressiveScan);
+			Console.WriteLine("Using DSP HLE                  : {0}", movieFile.UsingHLEDSP);
+			Console.WriteLine("Using fast disc speed          : {0}", movieFile.UsingFastDiscSpeed);
+			Console.WriteLine("CPU Core                       : {0}", movieFile.CPUCore);
+			Console.WriteLine("EFB access enabled             : {0}", movieFile.IsEFBAccessEnabled);
+			Console.WriteLine("EFB copies enabled             : {0}", movieFile.IsEFBCopiesEnabled);
+			Console.WriteLine("Using EFB to texture           : {0}", movieFile.UsingEFBToTexture);
+			Console.WriteLine("Using EFB copy cache           : {0}", movieFile.IsEFBCopyCacheEnabled);
+			Console.WriteLine("Emulate EFB format changes     : {0}", movieFile.IsEmulatingEFBFormatChanges);
+			Console.WriteLine("Using XFB                      : {0}", movieFile.UsingXFB);
+			Console.WriteLine("Using Real XFB                 : {0}", movieFile.UsingRealXFB);
+			Console.WriteLine("Using memory card              : {0}", movieFile.UsingMemoryCard);
+			Console.WriteLine("Create new memcard on playback : {0}", movieFile.UsingClearSaves);
+			Console.WriteLine("Bongos                         : {0}", movieFile.Bongos);
+			Console.WriteLine("Synchronize GPU                : {0}", movieFile.SyncGPU);
+			Console.WriteLine("Using NetPlay                  : {0}", movieFile.UsingNetplay);
+			Console.WriteLine("Second disc name               : {0}", movieFile.SecondDiscName);
+			Console.WriteLine("Revision                       : {0}", movieFile.GitRevision); // TODO
+			Console.WriteLine("DSP IROM hash                  : {0:X}", movieFile.DSPIROMHash);
+			Console.WriteLine("DSP coef hash                  : {0:X}", movieFile.DSPCoefHash);
+		}
+	}
+}
